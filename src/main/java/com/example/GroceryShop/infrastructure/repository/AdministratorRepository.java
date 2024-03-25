d309c99fa3c8f111023358e68e8c45e01b87a040
damian.figinski <figo96it@gmail.com>
2024-03-25 20:33:48 +0100

    Add logic for AdministratorRepository and UserRepository

--- src/main/java/com/example/GroceryShop/infrastructure/repository/AdministratorRepository.java
+++ src/main/java/com/example/GroceryShop/infrastructure/repository/AdministratorRepository.java
@@ -1,4 +1,21 @@
 package com.example.GroceryShop.infrastructure.repository;
 
-public class AdministratorRepository {
+import com.example.GroceryShop.domain.model.Administrator;
+import com.example.GroceryShop.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AdministratorRepository extends JpaRepository<Administrator, Long> {
+
+    Optional<Administrator> findById(String id);
+
+    Optional<Administrator> findUserByUsername(String username);
+
+    List<Administrator> findBySurname(String surname);
+
+    List<Administrator> findByName(String name);
 }

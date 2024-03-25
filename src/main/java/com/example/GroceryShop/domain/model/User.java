2180798c6e82b31fa8fe526d01c7dc13d9614a29
Grzegorz Pulawski <szkolanakrawedzi@gmail.com>
2024-03-25 20:17:47 +0100

    Userfirst

--- src/main/java/com/example/GroceryShop/domain/model/User.java
+++ src/main/java/com/example/GroceryShop/domain/model/User.java
@@ -1,6 +1,85 @@
 package com.example.GroceryShop.domain.model;
 
+import java.time.LocalDate;
+
 public class User {
+    private Long id;
+    private String name;
+    private String surname;
+    private LocalDate dateOfRegistration;
+    private LocalDate dateOfBorn;
+    private String email;
 
 
+    public User() {
+
+    }
+
+    public User(Long id, String name, String surname, LocalDate dateOfRegistration, LocalDate dateOfBorn) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.dateOfRegistration = dateOfRegistration;
+        this.dateOfBorn = dateOfBorn;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getDateOfRegistration() {
+        return dateOfRegistration;
+    }
+
+    public void setDateOfRegistration(LocalDate dateOfRegistration) {
+        this.dateOfRegistration = dateOfRegistration;
+    }
+
+    public LocalDate getDateOfBorn() {
+        return dateOfBorn;
+    }
+
+    public void setDateOfBorn(LocalDate dateOfBorn) {
+        this.dateOfBorn = dateOfBorn;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dateOfRegistration=" + dateOfRegistration +
+                ", dateOfBorn=" + dateOfBorn +
+                '}';
+    }
 }

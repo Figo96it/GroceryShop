9f1b2e3ba8b176b80f1652d1a86d49ba048d0c1f
Adam <adamfijak@gmail.com>
2024-03-25 19:11:07 +0000

    adam1 add new parameters for admin

--- src/main/java/com/example/GroceryShop/domain/model/Administrator.java
+++ src/main/java/com/example/GroceryShop/domain/model/Administrator.java
@@ -1,4 +1,47 @@
 package com.example.GroceryShop.domain.model;
 
 public class Administrator {
+
+
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Administrator(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }

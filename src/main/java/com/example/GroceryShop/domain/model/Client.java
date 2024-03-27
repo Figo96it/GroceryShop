de504a003fe60d137f36bd875e603589eb70fb05
damian.figinski <figo96it@gmail.com>
2024-03-27 20:51:57 +0100

    Add tests for RegistrationController

--- src/main/java/com/example/GroceryShop/domain/model/Client.java
+++ src/main/java/com/example/GroceryShop/domain/model/Client.java
@@ -47,4 +47,10 @@ public class Client {
     this.dateOfBorn = dateOfBorn;
     this.email = email;
   }
+
+  public Client(String username, String email, String password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
 }
---
+++ src/test/java/com/example/GroceryShop/application/controller/RegistrationControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.GroceryShop.application.controller;
+
+import com.example.GroceryShop.application.service.register.RegistrationService;
+import com.example.GroceryShop.domain.model.Client;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class RegistrationControllerTest {
+
+    private RegistrationController registrationController;
+
+    @Mock
+    private RegistrationService registrationService;
+
+    @BeforeEach
+    void setUp() {
+        registrationController = new RegistrationController(registrationService);
+    }
+
+    @Test
+    void showRegistrationPageReturnsRegister() {
+        String viewName = registrationController.showRegistrationPage();
+        assertEquals("register", viewName);
+    }
+
+    @Test
+    void registerRedirectsToLogin() {
+        Client client = new Client("John", "john@example.com", "password");
+        String redirectUrl = registrationController.register(client);
+        assertEquals("redirect:/login", redirectUrl);
+        verify(registrationService, times(1)).registerNewClient(client);
+    }
+}
